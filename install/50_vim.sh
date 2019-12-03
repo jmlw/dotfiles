c715c496c0a3b4bc85d2ef35b344775f134d07a4
Josh Wood <josh.m.l.wood@gmail.com>
2019-12-03 11:19:15 -0600

    update vim install

--- install/50_vim.sh
+++ install/50_vim.sh
@@ -1,6 +1,12 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/vim
 
+plugins=(
+  "vim-sensible|https://github.com/tpope/vim-sensible.git"
+  "editorconfig-vim|https://github.com/editorconfig/editorconfig-vim.git"
+  "vim-polyglot|https://github.com/sheerun/vim-polyglot.git"
+)
+
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
   #vim +PlugUpgrade +PlugUpdate +qall
@@ -8,3 +14,14 @@ if [[ "$(type -P vim)" ]]; then
   mkdir -p ~/.vim/autoload ~/.vim/bundle && \
   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
+
+for p in "${plugins[@]}"; do
+  name="$(echo "$p" | cut -d'|' -f 1)"
+  url="$(echo "$p" | cut -d'|' -f 2)"
+  echo "Installing $name from $url"
+  if [ -d "$DOTFILES/link/.vim/bundle/$name" ]; then
+    git -C "$DOTFILES/link/.vim/bundle/$name" pull
+  else
+    git clone "$url" "$DOTFILES/link/.vim/bundle/$name"
+  fi
+done
